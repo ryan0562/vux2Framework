2aa9f7291b79088ee21dc0373714fdc260f3bf11
王伟 <wangwei171@zuozh.com>
2018-03-23 15:06:23 +0800

    添加了loading过度

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -2,7 +2,7 @@
   <section>
     <group title="登录信息(账户跟密码为1,1)" label-width="100px">
       <x-input title="账户" placeholder="请输入登录账户" v-model="account"></x-input>
-      <x-input title="密码" placeholder="请输入登录账户" v-model="password" type="password" @on-enter="login()"></x-input>
+      <x-input title="密码" placeholder="请输入账户密码" v-model="password" type="password" @on-enter="login()"></x-input>
     </group>
     <box gap="20px 10px">
       <x-button type="primary" @click.native="login()">登录</x-button>
